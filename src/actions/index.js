82d6fae7afac21da0feb38c9c19d54535ebded2a
木畑永允 <nagamitsu@kibatahisashijounoMacBook-puro.local>
2018-08-28 01:20:35 +0900

    imprement reducer

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+const INCREMENT = 'INCREMENT'
+const DECREMENT ='DECREMENZT'
+
+export const increment =()=>({
+        type: INCREMENT
+    })
+
+export const decrement =()=>({
+        type: DECREMENT
+    })    
+
