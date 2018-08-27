07b4a5b9a2156ed145264fec0ba615a109bef252
木畑永允 <nagamitsu@kibatahisashijounoMacBook-puro.local>
2018-08-27 23:49:01 +0900

    jsx more

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React, { Component } from 'react';
 
+// class App extends Component {
+//   render() {
+//     const greeting = "Hi, Nagamitsu!"
+//     const dom = <h1 className="foo">{greeting}</h1>
+//     return dom;
+//   }
+// }
+
+// class App extends Component {
+//   render() {
+//     return <input type="text" onClick={()=>{console.log("I am Cliked")}}/>;
+//   }
+// }
+
 class App extends Component {
   render() {
-    return <h1>Hello World</h1>;
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={()=>{console.log("I am Cliked")}}/>
+      </React.Fragment>
+    ) 
+   
   }
 }
-
 export default App;
