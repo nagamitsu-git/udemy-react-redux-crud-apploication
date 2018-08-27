0e0ad5698983f739a30df225ae3243c3565e6d50
木畑永允 <nagamitsu@kibatahisashijounoMacBook-puro.local>
2018-08-28 00:55:59 +0900

    learn state

--- src/App.js
+++ src/App.js
@@ -1,60 +1,39 @@
-//import React, { Component } from 'react';
-import React from 'react';
-import PropTypes from 'prop-types'
 
-// class App extends Component {
-//   render() {
-//     const greeting = "Hi, Nagamitsu!"
-//     const dom = <h1 className="foo">{greeting}</h1>
-//     return dom;
-//   }
-// }
+import React, {Component} from 'react';
 
-// class App extends Component {
-//   render() {
-//     return <input type="text" onClick={()=>{console.log("I am Cliked")}}/>;
-//   }
-// }
+const App = ()=>(<Counter></Counter>)
 
-// class App extends Component {
-//   render() {
-//     return(
-//       <React.Fragment>
-//         <label htmlFor="bar">bar</label>
-//         <input type="text" onChange={()=>{console.log("I am Cliked")}}/>
-//       </React.Fragment>
-//     ) 
-   
-//   }
-// }
+class Counter extends Component
+{
+  constructor(props)
+  {
+    super(props)
+    //console.log(this.state)
+    this.state={ count:0 }
+  }
 
-const App = ()=>{
-  const profiles=[
-    {name: "Taro", age: 10},
-    {name: "Hanako", age: 5},
-    {name: "NoName", age: 1}
-  ]
-  return (
-        <div>
-          {
-            profiles.map((profile, index)=>{
-            return <User name={profile.name} age={profile.age} key={index}/>
-            })
-          }
-         </div>
-  ) 
+  handlePlusButton = ()=>{
+    console.log("handlePlusButton")
+    console.log(this.state.count)
+    this.setState({count: this.state.count + 1})
+  }
+  handleMinusButton = ()=>{
+    console.log("handleMinusButton")
+    console.log(this.state.count)
+    this.setState({count: this.state.count - 1})
+  }
+
+  render(){
+    //console.log(this.state)
+    console.log("call render")
+    return (
+      <React.Fragment>
+      <div>count:{this.state.count}</div>
+      <button onClick={this.handlePlusButton}>+1</button>
+      <button onClick={this.handleMinusButton}>+1</button>
+      </React.Fragment>
+    )
+  }
 }
 
-const User =(props) =>{
-  return <div>Hi, Iam {props.name} and {props.age} years old</div>
-}
-
-/* User.defaultProps ={
-  age:1
-} */
-
-User.propTypes ={
-  name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
 export default App;
